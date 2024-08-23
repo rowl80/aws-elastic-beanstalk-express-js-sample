4634990f92a8e096df6d795737cfb352b7802719
rowl80 <57596514+rowl80@users.noreply.github.com>
2024-08-23 11:28:43 -0600

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Lets GO!!!'));
+app.get('/', (req, res) => res.send('You are the heart and soul of this team.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
