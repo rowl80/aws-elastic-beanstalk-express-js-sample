0bc93f16921e82982e096261e24af18b020baf22
rowl80 <57596514+rowl80@users.noreply.github.com>
2024-12-05 09:36:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('You are the heart and soul of this team.'));
+app.get('/', (req, res) => res.send('You are my sunshine.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
